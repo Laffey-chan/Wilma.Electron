44576298603a858986e65a01877a791ecdc8cf36
Jaakko <jaakko@kronogra.fi>
2019-06-17 18:42:17 +0300

    made a login form
    It needs an update to the wilma package for it to work
     At the moment it only logs in and saves the SID

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';  
+import React from 'react';
+import wilma from 'wilma';
 import './App.css';
 import MenuBar from './components/MenuBar';
 import Login from './components/Login';
@@ -6,17 +7,31 @@ import Login from './components/Login';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = { SID: '', LoggedIn: false };
   }
   render() {
     return (
       <div className="App">
-      <MenuBar></MenuBar>
+        <MenuBar></MenuBar>
         <body>
-          <Login></Login>
+          <Login getSID={this.getSID.bind(this)}></Login>
         </body>
       </div>
     );
   }
+
+  async getSID(userName, passWord) {
+    var newSID = await wilma.LoginWilma(userName, passWord);
+    alert(newSID);
+    if (newSID !== ''){
+      this.setState({ SID: newSID, LoggedIn: true });
+      alert("Kirjautuminen onnistui \n SID: " + newSID);
+    }
+    else
+        alert("Kirjautuminen epäonnistui");
+  }
+
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,14 +3,39 @@ import React from 'react';
  class Login extends React.Component{
      constructor(props){
          super(props);
+
+         this.state = {username: '', password: ''};
+
+         this.handleChange = this.handleChange.bind(this);
+         this.handleSubmit = this.handleSubmit.bind(this);
+         this.login = this.login.bind(this);
      }
      render(){
          return(
-             <form>
-                 <input type="text"></input>
-                 <input type="submit"></input>
+             <form onSubmit={this.handleSubmit}>
+                 <input id="Username" type="text" placeholder='Käyttäjänimi' value={this.state.username} onChange={this.handleChange}></input>
+                 <input id="Password" type="text" placeholder='Salasana' value={this.state.password} onChange={this.handleChange}></input>
+                 <input type="submit" value="Kirjaudu"/>
              </form>
          );
+     }
+    login = () =>{
+        this.props.getSID(this.state.username, this.state.password);
+    }
+    handleSubmit(event){
+        this.login();
+         event.preventDefault();
+     }
+     handleChange(event){
+        if(event.target.id === "Username"){
+            this.setState({username: event.target.value});
+        }
+        else if(event.target.id === "Password"){
+            this.setState({password: event.target.value});
+        }
+        else{
+            alert("ree");
+        }
      }
  }
  export default Login;
